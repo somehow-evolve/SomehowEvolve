7186aec540f3d36566b83e51aa84c23df7560695
Игорь Щербаков <dj_jazz2010@mail.ru>
2024-07-31 00:07:17 +0300

    header+ logo

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,23 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+
+window.addEventListener('load', () => {
+  const button = document.querySelector('.js-burger');
+  const navMenu = document.querySelector('.js-nav-menu');
+  const headerWr = document.querySelector('.header__wrraper');
+  const header = document.querySelector('.header');
+
+  navMenu.classList.remove('is-active');
+  button.classList.remove('no-js');
+  headerWr.classList.remove('no-js');
+  navMenu.classList.remove('no-js');
+  header.classList.remove('no-js');
+
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    button.classList.toggle('is-active');
+    navMenu.classList.toggle('is-active');
+  });
+});
